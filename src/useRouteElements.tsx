2714b81610517ccffcf6ba8614d343aa4755390d
minhtoan1210 <nguyenvuminhtoan1999@gmail.com>
2024-01-22 08:02:29 +0700

    Setup Protected Route và Rejected Route trong React Router

--- src/useRouteElements.tsx
+++ src/useRouteElements.tsx
@@ -1,35 +1,75 @@
-import { useRoutes } from 'react-router-dom'
+import { Navigate, Outlet, useRoutes } from 'react-router-dom'
 import Login from './pages/Login'
-import Resgiter from './pages/Register'
 import RegisterLayout from './layouts/RegisterLayout'
 import ProductList from './pages/ProductList'
 import MainLayout from './layouts/MainLayout/MainLayout'
+import Register from './pages/Register'
 
+const isAuthenticated = false
+//Outlet dùng để truy xuất các thành phần con của chúng
+function ProtectedRoute() {
+  return isAuthenticated ? <Outlet /> : <Navigate to='/login' />
+}
+
+function RejectedRoute() {
+  return !isAuthenticated ? <Outlet /> : <Navigate to='/' />
+}
 export default function useRouteElements() {
   const routeElements = useRoutes([
     {
       path: '/',
+      // nhớ xác định thành phần chín bằng
+      index: true,
       element: (
         <MainLayout>
           <ProductList />
         </MainLayout>
       )
     },
+
     {
-      path: '/login',
-      element: (
-        <RegisterLayout>
-          <Login />
-        </RegisterLayout>
-      )
+      path: '',
+      element: <RejectedRoute />,
+      children: [
+        {
+          path: 'login',
+          element: (
+            <RegisterLayout>
+              <Login />
+            </RegisterLayout>
+          )
+        },
+        {
+          path: 'register',
+          element: (
+            <RegisterLayout>
+              <Register />
+            </RegisterLayout>
+          )
+        }
+      ]
     },
     {
-      path: '/register',
-      element: (
-        <RegisterLayout>
-          <Resgiter />
-        </RegisterLayout>
-      )
+      path: '',
+      element: <ProtectedRoute />,
+      children: [
+        {
+          path: 'login',
+          element: (
+            <RegisterLayout>
+              <Login />
+            </RegisterLayout>
+          )
+        },
+        {
+          path: 'register',
+          element: (
+            <RegisterLayout>
+              <Register />
+            </RegisterLayout>
+          )
+        }
+      ]
     }
   ])
   return routeElements
