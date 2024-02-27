dc214de6308db0dadb6e675c254b42ce51e973b8
minhtoan1210 <nguyenvuminhtoan1999@gmail.com>
2024-02-28 06:37:05 +0700

    vitest

---
+++ src/utils/__test__/utils.test.ts
@@ -0,0 +1,46 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { AxiosError } from 'axios'
+import { describe, it, expect } from 'vitest'
+import { isAxiosError, isAxiosUnprocessableEntityError } from '../utils'
+import { HttpStatusCode } from 'src/constants/HttpStatusCode'
+
+// describe dùng để mô tả tập hợp các ngữ cảnh
+// hoặc 1 đơn vị cần test: Ví dụ function, component
+describe('isAxiosError', () => {
+  // it dùng để ghi chú trường hợp cần test
+  it('isAxiosError trả về boolean', () => {
+    // expect dùng để mong đợi giá trị trả về
+    expect(isAxiosError(new Error())).toBe(false)
+    expect(isAxiosError(new AxiosError())).toBe(true)
+  })
+})
+
+describe('isAxiosUnprocessableEntityError', () => {
+  it('isAxiosUnprocessableEntityError trả về boolean', () => {
+    expect(isAxiosUnprocessableEntityError(new Error())).toBe(false)
+    expect(
+      isAxiosUnprocessableEntityError(
+        new AxiosError(undefined, undefined, undefined, undefined, {
+          status: HttpStatusCode.InternalServerError,
+          data: null
+        } as any)
+      )
+    ).toBe(false)
+    expect(
+      isAxiosUnprocessableEntityError(
+        new AxiosError(undefined, undefined, undefined, undefined, {
+          status: HttpStatusCode.UnprocessableEntity,
+          data: null
+        } as any)
+      )
+    ).toBe(true)
+    expect(
+      isAxiosUnprocessableEntityError(
+        new AxiosError(undefined, undefined, undefined, undefined, {
+          status: HttpStatusCode.AlreadyReported,
+          data: null
+        } as any)
+      )
+    ).toBe(false)
+  })
+})
